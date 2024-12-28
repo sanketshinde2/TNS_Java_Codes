4e8959a4a5eed460665259799f4328b6a168d23a
Sanket Shinde <sanketbalasoshinde@gmail.com>
2024-12-28 19:06:10 +0530

    Initial commit

---
+++ dayfive/src/singleinheritance/SLInheritanceDemo.java
@@ -0,0 +1,13 @@
+package singleinheritance;
+
+public class SLInheritanceDemo {
+
+	
+	public static void main(String[] args) {
+		
+		Student st = new Student("sanket", "8080808080","Pune", "9146393025",02, "G.H.Raisoni");
+		System.out.println(st);
+
+	}
+
+}
